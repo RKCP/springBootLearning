7ff75894089168cb3668a2c071a9129c62d4b9c9
RAPHAEL PETERS <RKCPETERS@gmail.com>
2022-11-24 15:25:12 +0000

    Skeleton Spring project.

---
+++ fullstackSpring/src/main/java/com/raphael/fullstackSpring/FullstackSpringApplication.java
@@ -0,0 +1,13 @@
+package com.raphael.fullstackSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackSpringApplication.class, args);
+	}
+
+}
---
+++ fullstackSpring/src/test/java/com/raphael/fullstackSpring/FullstackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphael.fullstackSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
