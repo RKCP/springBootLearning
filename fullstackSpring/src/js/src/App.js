79f2fddcf76cb2dbec88880deeca9ed122239bcf
RAPHAEL PETERS <RKCPETERS@gmail.com>
2023-01-06 14:04:54 +0000

    Student data in React Table using Ant module.

--- fullstackSpring/src/js/src/App.js
+++ fullstackSpring/src/js/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import Container from './Container';
 import { render } from '@testing-library/react';
 import { Component } from 'react';
-import { getAllStudents } from './client'
+import { getAllStudents } from './client';
+import { Avatar, Table, Spin } from 'antd';
 
 class App extends Component{
 
   state = {
-    students: []
+    students: [],
+    isFetching: false
   }
 
   componentDidMount () {
@@ -14,11 +17,17 @@ class App extends Component{
   }
 
   fetchStudents = () => {
+    this.setState({
+      isFetching: true
+    })
     getAllStudents()
     .then(res => res.json()
     .then(students => {
       console.log(students);
-      this.setState({students}) // pass our state the students object we get from the fetch method
+      this.setState({
+        students,
+        isFetching: false // when we get the students, set the fetching state to false
+      }) // pass our state the students object we get from the fetch method
     }));
   }
 
@@ -28,18 +37,54 @@ class App extends Component{
 
     if (students && students.length) { // if we have students, and it is not empty
       
-      return students.map((student, index) => { // we return the actual students
-        return (
-          <div key={index}>
-            <h2>{student.studentId}</h2>
-            <p>{student.firstName}</p>
-            <p>{student.lastName}</p>
-            <p>{student.gender}</p>
-            <p>{student.email}</p>
-          </div>
-        )
-      })
+      const columns = [
+        {
+          title: '',
+          key: 'avatar',
+          render: (text, student) => ( // if you want to render custom components inside of your column, you need to use render
+            <Avatar size='large'>
+              {`${student.firstName.charAt(0).toUpperCase()}${student.lastName.charAt(0).toUpperCase()}`}
+            </Avatar>
+          )
+        },
+        {
+          title: 'Student Id',
+          dataIndex: 'studentId',
+          key: 'studentId'
+        },
+        {
+          title: 'First Name',
+          dataIndex: 'firstName',
+          key: 'firstName'
+        },
+        {
+          title: 'Last Name',
+          dataIndex: 'lastName',
+          key: 'lastName'
+        },
+        {
+          title: 'Email',
+          dataIndex: 'email',
+          key: 'email'
+        },
+        {
+          title: 'Gender',
+          dataIndex: 'gender',
+          key: 'gender'
+        }
+      ];
+
+      return (
+        <Container>
+          <Table 
+            dataSource={ students } 
+            columns={ columns } 
+            pagination= { false }
+            rowKey='studentId'/>
+        </Container>
+      );
     }
+
     // else
     return <h1>No Students found</h1>
   }
