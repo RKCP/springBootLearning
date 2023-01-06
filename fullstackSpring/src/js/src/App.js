1a0ca229dd1432aba5ee6dba36f80019a0eb4c07
RAPHAEL PETERS <RKCPETERS@gmail.com>
2023-01-06 13:17:50 +0000

    Changes to React side to display students on Webpage (not in console)

--- fullstackSpring/src/js/src/App.js
+++ fullstackSpring/src/js/src/App.js
@@ -1,16 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
 import { Component } from 'react';
 import { getAllStudents } from './client'
 
 class App extends Component{
-  render() {
-    getAllStudents().then(res => res.json().then(students => {
-      console.log("1")
+
+  state = {
+    students: []
+  }
+
+  componentDidMount () {
+    this.fetchStudents(); // call method when the component mounts
+  }
+
+  fetchStudents = () => {
+    getAllStudents()
+    .then(res => res.json()
+    .then(students => {
       console.log(students);
+      this.setState({students}) // pass our state the students object we get from the fetch method
     }));
-    return <h1>Hello World Spring Boot & React</h1>
+  }
+
+  render() {
+
+    const { students } = this.state; // state contains the actual student
+
+    if (students && students.length) { // if we have students, and it is not empty
+      
+      return students.map((student, index) => { // we return the actual students
+        return (
+          <div key={index}>
+            <h2>{student.studentId}</h2>
+            <p>{student.firstName}</p>
+            <p>{student.lastName}</p>
+            <p>{student.gender}</p>
+            <p>{student.email}</p>
+          </div>
+        )
+      })
+    }
+    // else
+    return <h1>No Students found</h1>
   }
 }
 
